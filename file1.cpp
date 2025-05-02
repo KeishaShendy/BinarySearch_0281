d01078f4535ef6bfe88bcacac011f2cb7ca1cdec
keishashendy <shendyarrafli7@gmail.com>
2025-05-02 09:42:57 +0700

    menambahkan fungsi input untuk mengisi array

--- file1.cpp
+++ file1.cpp
@@ -5,3 +5,27 @@ int elemen[10];
 int npanjang;
 int x;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array: ";
+        cin >> npanjang;
+        if (npanjang <= 10)
+            break;
+        else
+        {
+            cout << "\nArray maksimal 20 elemen.\n";
+        }
+    }
+
+    cout << "===================" << endl;
+    cout << "Masukan elemen arry" << endl;
+    cout << "===================" << endl;
+
+    for (int i = 0; i < npanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << "=";
+        cin >> elemen[i];
+    }
+}
